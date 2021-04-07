6bf1a4e7cae37b954a4fa610d9e39bd002f2c5bf
hhllbao93 <32312472+hhllbao93@users.noreply.github.com>
2021-04-07 13:44:12 -0700

    Delete v1.1 directory

--- v1.1/ssib4_src/module_comsdef.F90
+++
@@ -1,7 +0,0 @@
-MODULE module_comsdef 
-! module template 
-   integer, parameter:: LONF2S= 0
-   integer, parameter:: LONF2E= 60
-   integer, parameter:: LATG2S= 35
-   integer, parameter:: LATG2E= 110
-END MODULE
